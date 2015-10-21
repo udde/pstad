8000e496018a8b4e369148952baecc75180109ac
Alexander <alexander.uddfeldt@gmail.com>
2015-10-21 15:51:29 +0200

    testing materials

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var stats = new Stats();
+stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
+
+// align top-left
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+
+document.body.appendChild( stats.domElement );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+controls = new THREE.OrbitControls( camera );
+controls.addEventListener( 'change', render );
+
+var geometry = new THREE.TorusKnotGeometry( 3 , 1, 100, 16 );
+var material = new THREE.MeshPhongMaterial( { ambient: 0x999999, color: 0xbbbbbb, specular: 0xeeeeee, shininess: 30 } );
+
+var light = new THREE.DirectionalLight(0xeeeeee, 1, 100);
+light.position.set(10,0,50);
+scene.add(light);
+
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 20;
 camera.position.x = 1;
 
 function render() {
+	stats.begin();
+
+	stats.end();
 	requestAnimationFrame( render );
 	renderer.render( scene, camera );
 }
